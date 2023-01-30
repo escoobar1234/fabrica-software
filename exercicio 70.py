f51dccee193bda805263092858fd3a2e83d9e589
escoobar1234 <119597722+escoobar1234@users.noreply.github.com>
2023-01-30 08:44:34 -0400

    primeiro mommit

--- exercicio 70.py
+++ exercicio 70.py
@@ -2,4 +2,19 @@ nome = input("digite o nome do atleta:")
 salto = float(input("digite a altura do salto:"))
 maior = salto
 menor = salto
+cont = 0
 soma = 0
+soma = salto + 1
+while cont < 4:
+    salto = float(input("digite a altura do salto:"))
+    if salto < menor:
+        menor = salto
+    elif salto > maior:
+        maior = salto
+    soma = soma + salto
+    cont = cont + 1
+print(f"O melhor salto foi:{maior}")
+print(f"O pior salto foi :{menor}")
+print(f"A media dos saltos foi:{soma/5:.2f} ")
+print("resultado final:")
+print(f"{nome}:{soma/5:.2f}")
